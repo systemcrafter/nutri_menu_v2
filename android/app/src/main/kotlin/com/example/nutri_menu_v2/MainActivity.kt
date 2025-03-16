bcc66906f9299998f0cb7138b7ad115ca63c9da4
systemcrafter <edwin.mejia@outlook.com>
2025-03-16 09:24:32 -0600

    Inicio de projecto

---
+++ android/app/src/main/kotlin/com/example/nutri_menu_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nutri_menu_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Hola Mundo Ed',
+        home: RecipeBook());
+  }
+}
+
+class RecipeBook extends StatelessWidget {
+  const RecipeBook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text(
+          'Libro de Recetas',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
